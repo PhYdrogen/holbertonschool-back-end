dd4606acc2b069fd032e057b54b06618c16b3dfd
Gabriel <fossecruor@gmail.com>
2023-04-05 10:48:35 -0700

    using json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,15 +2,16 @@
 """ using requests for api """
 
 if __name__ == "__main__":
+    import json
     import requests
     from sys import argv
-
 
     if len(argv) <= 1:
         exit(1)
     user_id = int(argv[1])
 
-    ru_json = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}").json()
+    ru_json = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{user_id}").json()
     rt_json = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
     LIST_TASKS = []
@@ -30,4 +31,4 @@ if __name__ == "__main__":
 
     userDict[f"{user_id}"] = LIST_TASKS
     with open(f"{user_id}.json", 'w') as f:
-        f.write(str(userDict))
+        f.write(json.dumps(userDict))
