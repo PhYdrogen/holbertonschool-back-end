2a98bf8da13a24a1aa9f882990f6efd4ed29c68c
Gabriel <fossecruor@gmail.com>
2023-04-05 10:49:07 -0700

    pep 8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-
     if len(argv) <= 1:
         exit(1)
     user_id = int(argv[1])
 
-    ru_json = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}").json()
+    ru_json = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{user_id}").json()
     rt_json = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
     LIST_TITLE = []
@@ -24,8 +24,9 @@ if __name__ == "__main__":
         if not task.get("completed") and task.get("userId") == user_id:
             TOTAL_NUMBER_OF_TASKS += 1
 
-    print(f1 := f"Employee {EMPLOYEE_NAME} is done with tasks",end="")
-    print(f2 := f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS+NUMBER_OF_DONE_TASKS}):")
+    print(f1 := f"Employee {EMPLOYEE_NAME} is done with tasks", end="")
+    print(f2 := f"({NUMBER_OF_DONE_TASKS}/\
+          {TOTAL_NUMBER_OF_TASKS+NUMBER_OF_DONE_TASKS}):")
     for title in LIST_TITLE:
         print(f"\t {title}")
-        
+
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-
     if len(argv) <= 1:
         exit(1)
     user_id = int(argv[1])
 
-    ru_json = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}").json()
+    ru_json = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{user_id}").json()
     rt_json = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
     LIST_TITLE = []
@@ -22,6 +22,9 @@ if __name__ == "__main__":
             if task.get("userId") == user_id:
                 TASK_COMPLETED_STATUS = task.get("completed")
                 TASK_TITLE = task.get("title")
-                f.write(f'"{user_id}","{EMPLOYEE_NAME}","{TASK_COMPLETED_STATUS}","{TASK_TITLE}"\n')
+                f.write(
+                    f'"{user_id}",\
+                    "{EMPLOYEE_NAME}",\
+                    "{TASK_COMPLETED_STATUS}",\
+                    "{TASK_TITLE}"\n')
 
-        
