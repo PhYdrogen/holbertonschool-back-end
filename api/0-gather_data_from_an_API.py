6703e9ec1a084199123f4cab1da8495452580f31
Gabriel <Fossecruor@gmail.com>
2023-04-05 13:59:50 +0200

    adding write to file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     from sys import argv
 
 
+    if len(argv) <= 1:
+        exit(1)
     user_id = int(argv[1])
 
     ru_json = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}").json()
@@ -22,7 +24,10 @@ if __name__ == "__main__":
         if not task.get("completed") and task.get("userId") == user_id:
             TOTAL_NUMBER_OF_TASKS += 1
 
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks",end="")
-    print(f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS+NUMBER_OF_DONE_TASKS}):")
+    print(f1 := f"Employee {EMPLOYEE_NAME} is done with tasks",end="")
+    print(f2 := f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS+NUMBER_OF_DONE_TASKS}):")
     for title in LIST_TITLE:
         print(f"\t {title}")
+        
+    with open("student_output", 'w') as f:
+        f.write(f1 + f2)
