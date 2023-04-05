f2437bef5af8f170810fc3316ecef9067992ec07
Gabriel <Fossecruor@gmail.com>
2023-04-05 12:39:45 +0200

    Vals carry

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" using requests for api """
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+
+    if len(argv) <= 1:
+        exit(1)
+    user_id = int(argv[1])
+
+    r_user = requests.get("https://jsonplaceholder.typicode.com/users")
+    r_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    # print(ru_json := r_user.json())
+    # print(rt_json := r_todo.json())
+
+    ru_json = r_user.json()
+    rt_json = r_todo.json()
+
+    LIST_TITLE = []
+    for user in ru_json:
+        userId = user.get("id")
+        TOTAL_NUMBER_OF_TASKS = 0
+        NUMBER_OF_DONE_TASKS = 0
+        LIST_TITLE = []
+        if user_id == userId:
+            print(f"Employee {user.get('name')} is done with task",end="")
+            for task in rt_json:
+                if task.get("completed") and task.get("userId") == userId:
+                    NUMBER_OF_DONE_TASKS += 1
+                    LIST_TITLE.append(task.get("title"))
+                if not task.get("completed") and task.get("userId") == userId:
+                    TOTAL_NUMBER_OF_TASKS += 1
+
+            print(f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS+NUMBER_OF_DONE_TASKS}):")
+            for title in LIST_TITLE:
+                print(f"\t {title}")
