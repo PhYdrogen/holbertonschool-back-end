c1c929dfe8eb44df37502f9e5349c3f0f6ce0af8
Gabriel <Fossecruor@gmail.com>
2023-04-05 13:46:56 +0200

    changed code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,25 +10,21 @@ if __name__ == "__main__":
         exit(1)
     user_id = int(argv[1])
 
-    ru_json = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    ru_json = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}").json()
     rt_json = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
     LIST_TITLE = []
-    for user in ru_json:
-        userId = user.get("id")
-        TOTAL_NUMBER_OF_TASKS = 0
-        NUMBER_OF_DONE_TASKS = 0
-        LIST_TITLE = []
-        if user_id == userId:
-            EMPLOYEE_NAME = user.get('name')
-            print(f"Employee {EMPLOYEE_NAME} is done with tasks",end="")
-            for task in rt_json:
-                if task.get("completed") and task.get("userId") == userId:
-                    NUMBER_OF_DONE_TASKS += 1
-                    LIST_TITLE.append(task.get("title"))
-                if not task.get("completed") and task.get("userId") == userId:
-                    TOTAL_NUMBER_OF_TASKS += 1
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
+    EMPLOYEE_NAME = ru_json.get('name')
+    for task in rt_json:
+        if task.get("completed") and task.get("userId") == user_id:
+            NUMBER_OF_DONE_TASKS += 1
+            LIST_TITLE.append(task.get("title"))
+        if not task.get("completed") and task.get("userId") == user_id:
+            TOTAL_NUMBER_OF_TASKS += 1
 
-            print(f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS+NUMBER_OF_DONE_TASKS}):")
-            for title in LIST_TITLE:
-                print(f"\t {title}")
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks",end="")
+    print(f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS+NUMBER_OF_DONE_TASKS}):")
+    for title in LIST_TITLE:
+        print(f"\t {title}")
